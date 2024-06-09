8fb33caa5c2988c5cfaef8886eb32c4fe8e68bf3
r-miyashita <rmiyashita338@gmail.com>
2024-06-09 18:24:53 +0900

    refactor 不要コード削除

--- createUpdateQuery.py
+++ createUpdateQuery.py
@@ -1,10 +1,9 @@
 """
 .csvからExcelを作成する。
-シート作成
-csv転記
-sql生成（excel式）
-samarry生成
-nullチェック
+    csv転記(ファイル数に応じてシート追加)
+    sql生成（excel式埋め込み）
+    クライアントシステムでSQL一括実行するためのコピペフィールド生成(excel式埋め込み)
+    nullの警告
 """
 import shutil
 import re
@@ -12,14 +11,16 @@ import pandas as pd
 # from openpyxl import Workbook
 from pathlib import Path
 
-
+'''------------------------------
+前準備
+------------------------------'''
 root = Path('.')
 input_dir = root / 'in_updateQuery'
-inputs = sorted(list(input_dir.glob('**/*.csv')))
+input_files = sorted(list(input_dir.glob('**/*.csv')))
 output_dir = root / 'out_updateQuery'
 output_file = f'{output_dir}/test.xlsx'
 
-# input 存在チェック
+# 入力元: 存在チェック
 exit_msg = '処理を終了します。'
 err_reasons = {
     'dir_exists': f'{input_dir}が存在しません。ディレクトリを作成し、入力元となるcsvファイルを格納してください。',
@@ -29,18 +30,18 @@ err_reasons = {
 if not input_dir.exists():
     print(f'{err_reasons['dir_exists']} \n {exit_msg}')
     exit()
-elif not inputs:
+elif not input_files:
     print(f'{err_reasons['file_exists']} \n {exit_msg}')
     exit()
 
-# 出力先の初期化
+# 出力先: 初期化
 if output_dir.exists():
     shutil.rmtree(output_dir)
 output_dir.mkdir()
 
 
 '''------------------------------
-csv転記
+csv >> excel書き出し(シート追記)
 ------------------------------'''
 
 
@@ -51,24 +52,20 @@ def createSheetTitle(csvFile):
 
 
 ws_list = []
-for idx, file in enumerate(inputs):
+for idx, file in enumerate(input_files):
     title = createSheetTitle(file)
     ws_list.append(title)
 
-df_list = []
-new_url_list = []
-for idx, file in enumerate(inputs):
+for idx, file in enumerate(input_files):
 
     # 1行目から新URLを取得
     df_head = pd.read_csv(file, header=None, nrows=1)
     new_url = df_head.iloc[0, 0]
-    new_url_list.append(new_url)
 
     # 2行目以降からヘッダーとデータを取得
     df = pd.read_csv(file, header=1)
     df_trimmed = df.replace(
         r'(^[\'|\"]{1}[\s|\t]*|[\s|\t]*[\'|\"]{1}$)', '', regex=True)
-    df_list.append(df_trimmed)
 
     if idx == 0:
         df_trimmed.to_excel(output_file, sheet_name=ws_list[idx])
